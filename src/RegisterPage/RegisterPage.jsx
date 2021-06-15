cfdb24af9ad140f69440e5826ecfe2483e5ce8a0
Peter Paul.Clemente <72077161+Peterpaulclemente@users.noreply.github.com>
2021-06-15 17:22:38 +0800

    Add files via upload

---
+++ src/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+function RegisterPage() {
+    const [user, setUser] = useState({
+        Name: '',
+       Course: '',
+        username: '',
+        password: ''
+    });
+    const [submitted, setSubmitted] = useState(false);
+    const registering = useSelector(state => state.registration.registering);
+    const dispatch = useDispatch();
+
+    // reset login status
+    useEffect(() => {
+        dispatch(userActions.logout());
+    }, []);
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setUser(user => ({ ...user, [name]: value }));
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        setSubmitted(true);
+        if (user.firstName && user.Course && user.username && user.password) {
+            dispatch(userActions.register(user));
+        }
+    }
+
+    return (
+        <div className="col-lg-8 offset-lg-2">
+            <h2>Register</h2>
+            <form name="form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>First Name</label>
+                    <input type="text" name="firstName" value={user.firstName} onChange={handleChange} className={'form-control' + (submitted && !user.firstName ? ' is-invalid' : '')} />
+                    {submitted && !user.firstName &&
+                        <div className="invalid-feedback">First Name is required</div>
+                    }
+                </div>
+                <div className="form-group">
+                    <label>Course</label>
+                    <input type="text" name="Course" value={user.Course} onChange={handleChange} className={'form-control' + (submitted && !user.Course ? ' is-invalid' : '')} />
+                    {submitted && !user.Course &&
+                        <div className="invalid-feedback">Course is required</div>
+                    }
+                </div>
+                <div className="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" value={user.username} onChange={handleChange} className={'form-control' + (submitted && !user.username ? ' is-invalid' : '')} />
+                    {submitted && !user.username &&
+                        <div className="invalid-feedback">Username is required</div>
+                    }
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" value={user.password} onChange={handleChange} className={'form-control' + (submitted && !user.password ? ' is-invalid' : '')} />
+                    {submitted && !user.password &&
+                        <div className="invalid-feedback">Password is required</div>
+                    }
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-primary">
+                        {registering && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                        Register
+                    </button>
+                    <Link to="/login" className="btn btn-link">Cancel</Link>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export { RegisterPage };
